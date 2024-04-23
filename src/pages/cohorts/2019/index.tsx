3021d7af442586996b7b64c8c3eb1c18062052c2
preciouskayili <preciouskayili@gmail.com>
2024-04-23 03:08:36 +0100

    KCCA Complete

--- src/pages/cohorts/2019/index.tsx
+++ src/pages/cohorts/2019/index.tsx
@@ -1,11 +1,11 @@
 import Footer from "../../../../components/Footer";
 import Nav from "../../../../components/Nav";
 
-export default function Cohort3() {
+export default function Cohort1() {
   return (
     <>
       <Nav />
-      <div className="relative bg-[url('/images/kcca9.JPG')] h-[100vh] bg-cover bg-center overflow-hidden">
+      <div className="relative bg-[url('/images/front2.jpg')] h-[100vh] bg-cover bg-top overflow-hidden">
         <div
           className="absolute top-[1px] left-0 right-0 bottom-0 min-h-[100vh] z-[1] bg-black"
           style={{
@@ -15,7 +15,7 @@ export default function Cohort3() {
         />
         <div className="relative z-[100] flex flex-col justify-center items-center text-white h-full px-[20px]">
           <h3 className="font-[600] font-montserrat text-[30px] md:text-[64px] leading-[40px] md:leading-[80px] capitalize md:w-[525px] text-center">
-            KCCA 2021 Cohort
+            KCCA 2019 Cohort
           </h3>
         </div>
       </div>
