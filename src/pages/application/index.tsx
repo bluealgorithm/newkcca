6416ac96572801e017ca77ae3f8960db57a6334f
Akachukwu <egbolucheakachukwu@gmail.com>
2024-06-20 17:40:07 +0100

    Added Spinner

--- src/pages/application/index.tsx
+++ src/pages/application/index.tsx
@@ -18,6 +18,24 @@ import {
 import { toast } from "sonner";
 import { useRouter } from "next/router";
 
+const Spinner = () => (
+  <svg className="animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24">
+    <circle
+      className="opacity-25"
+      cx="12"
+      cy="12"
+      r="10"
+      stroke="currentColor"
+      strokeWidth="4"
+    ></circle>
+    <path
+      className="opacity-75"
+      fill="currentColor"
+      d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+    ></path>
+  </svg>
+);
+
 const formSchema = z.object({
   emailAddress: z.string().email("Enter a Valid Email Address"),
   age: z
@@ -136,7 +154,7 @@ const RegistrationForm = () => {
         toast.success("Application Submitted Successfully, Check Your Email!", {
           style: { background: "green", color: "white" },
           className: "my-toast",
-          duration: 4000,
+          duration: 3000,
         });
         router.push("/");
       },
@@ -145,7 +163,7 @@ const RegistrationForm = () => {
         toast.error("An error occurred while submitting the application.", {
           style: { background: "red", color: "white" },
           className: "my-toast",
-          duration: 4000,
+          duration: 3000,
         });
       },
     });
@@ -594,9 +612,19 @@ const RegistrationForm = () => {
           <div className="text-center mt-8">
             <button
               type="submit"
-              className="px-6 py-3 bg-[#63CA97] text-white font-semibold rounded-md focus:outline-none hover:bg-[#579d7e] transition-colors duration-300"
+              disabled={isPending}
+              className={`px-6 py-3 bg-[#63CA97] text-white font-semibold rounded-md focus:outline-none hover:bg-[#579d7e] transition-colors duration-300 flex items-center justify-center ${
+                isPending ? "opacity-50 cursor-not-allowed" : ""
+              }`}
             >
-              Apply Now
+              {isPending ? (
+                <>
+                  <Spinner />
+                  Submitting...
+                </>
+              ) : (
+                "Apply Now"
+              )}
             </button>
           </div>
         </form>
